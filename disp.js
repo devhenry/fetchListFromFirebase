15a45d72983e9f2e54284c4e8c09b91455c800d3
devHenry <72233421+devhenry@users.noreply.github.com>
2021-06-15 12:57:34 +0000

    Add files via upload

---
+++ disp.js
@@ -0,0 +1,66 @@
+
+    
+    var firebaseConfig = {
+    apiKey: "AIzaSyBv-XluD3Ba5tWl10yfjMJqXCZfyXivW6g",
+    authDomain: "mycrud-5369e.firebaseapp.com",
+    databaseURL: "https://mycrud-5369e-default-rtdb.firebaseio.com",
+    projectId: "mycrud-5369e",
+    storageBucket: "mycrud-5369e.appspot.com",
+    messagingSenderId: "727341125909",
+    appId: "1:727341125909:web:06bd30ee2423fb72a2191c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+
+  var stNo = 0;
+
+  function addItemsToList(name,rollNo,sec,gen){
+    var ul = document.getElementById('list');
+    var header = document.createElement('h2');
+
+    var stName = document.createElement('li');
+    var stRollNo = document.createElement('li');
+    var stSec = document.createElement('li');
+    var stGen = document.createElement('li');
+
+    header.innerHTML = 'Student -'+(++stNo);
+
+
+    stName.innerHTML = 'Name:' +name;
+    stRollNo.innerHTML = 'Roll No:' +rollNo;
+    stSec.innerHTML = 'Section:' +sec;
+    stGen.innerHTML = 'Gender:' +gen;
+
+    ul.appendChild(header);
+    ul.appendChild(stName );
+    ul.appendChild(stRollNo);
+    ul.appendChild( stSec);
+    ul.appendChild(stGen);
+    
+
+
+  }
+
+
+  function fetchAllData(){
+      firebase.database().ref('student').once('value',function(snapshot){
+
+        snapshot.forEach(
+               function(ChildSnapshot){
+                 let name = ChildSnapshot.val().NameOfStudent;
+                 let roll = ChildSnapshot.val().RollNumber;
+                 let sec = ChildSnapshot.val().Section;
+                 let gen = ChildSnapshot.val().Gender;
+                 addItemsToList(name,roll,sec,gen);
+
+               }
+
+        );
+      });
+      
+
+  }
+window.onload(fetchAllData());
+   
